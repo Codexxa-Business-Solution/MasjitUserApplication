8ab2f22e4b46b672b2aed679ce0f4de2bb0d9ed3
SayyadAaysha123 <aaysha@codexxa.in>
2022-09-28 17:23:29 +0530

    solved login screen

--- lib/user_login_screen.dart
+++ lib/user_login_screen.dart
@@ -3,6 +3,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:masjiduserapp/common.color.dart';
 import 'package:masjiduserapp/enter_mobile_number.dart';
 import 'package:masjiduserapp/masjit_user_app_api/masjit_app_responce_model/userLoginResponseModel.dart';
+import 'package:masjiduserapp/size_config.dart';
 import 'package:masjiduserapp/user_parent_tab_bar.dart';
 import 'package:masjiduserapp/user_registration.dart';
 import 'package:http/http.dart' as http;
@@ -21,7 +22,34 @@ class _LoginScreenState extends State<LoginScreen> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   Future<UserLoginResponseModel>? result;
+  final _formKey = GlobalKey<FormState>();
+  final _formKeyPassword = GlobalKey<FormState>();
 
+  String? validatepass(value) {
+    if (value!.isEmpty) {
+      return 'Area cannot be empty';
+    } else if (value.length < 3) {
+      return 'Area must be at least 3 characters long.';
+    }
+    return null;
+  }
+  Future<UserLoginResponseModel>? resultt;
+
+  validate() {
+    if (_formKey.currentState!.validate()) {
+      print("validated");
+             resultt = getLoginUsers();
+          resultt?.then((value) {
+              var box = Hive.box(kBoxName);
+          box.put(kToken, value.data?.token);
+          print("token ${box.get("token")}");
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => ParentTabBarScreen()));
+        });
+
+    } else {
+      print("not validated");
+    }}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,7 +59,7 @@ class _LoginScreenState extends State<LoginScreen> {
         children: [
           getTitleLayout(),
           getEmailField(),
-          getPasswordField(),
+          //getPasswordField(),
           getLoginButton(),
           getBottomText()
         ],
@@ -58,40 +86,149 @@ class _LoginScreenState extends State<LoginScreen> {
             padding: EdgeInsets.only(top: 18),
             child: Text("LOGIN",
               style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  letterSpacing: 3
+
+                    fontSize: 18,
+                    fontFamily: 'Roboto_Medium',
+                    //letterSpacing: parentWidth * 0.003,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white),
+                  /*letterSpacing: 3*/
               ),),
-          ),
+
         ],
       ),
     );
   }
 
   Widget getEmailField() {
-    return Padding(
-      padding: const EdgeInsets.only(top: 100, left: 15, right: 15),
-      child: TextFormField(
-        controller: emailController,
-        keyboardType: TextInputType.emailAddress,
-        decoration: const InputDecoration(
-          label: Text('Email Id'),
-          border: OutlineInputBorder(),
-        ),
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 80, left: 15, right: 15),
+            child: TextFormField(
+              controller: emailController,
+              keyboardType: TextInputType.emailAddress,
+              validator: (String? value) {
+                if (value!.isEmpty) {
+                  return 'email Field Is Required';
+                } /*else if (emailController.text.length < 6) {
+                  return 'Password Must Be 6 Character';*/
+
+                return null;
+              },
+                decoration: InputDecoration(
+                    labelText: 'Email',
+                    labelStyle: TextStyle(
+                      color: CommonColor.REGISTRARTION_COLOR
+                    ),
+                    contentPadding: const EdgeInsets.all(14),
+                    isDense: true,
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            width: 1,
+                            color: CommonColor.REGISTRARTION_COLOR),
+                        borderRadius: BorderRadius.circular(10.0)),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            width: 1,
+                            color: CommonColor.REGISTRARTION_COLOR),
+                        borderRadius: BorderRadius.circular(10.0)),
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            width: 1,
+                            color: CommonColor.REGISTRARTION_COLOR),
+                        borderRadius: BorderRadius.circular(10.0)),
+                    hintStyle: TextStyle(
+                      fontFamily: "Roboto_Regular",
+                      fontSize: SizeConfig.blockSizeHorizontal * 4.0,
+                        color: CommonColor.REGISTRARTION_TRUSTEE
+                    ))
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20, left: 15, right: 15),
+            child: TextFormField(
+                controller: passwordController,
+                obscureText: true,
+                validator: (String? value) {
+                  if (value!.isEmpty) {
+                    return 'Password Field Is Required';
+
+                  }
+                  return null;
+                },
+
+                decoration: InputDecoration(
+                    labelText: 'Password',
+
+                    contentPadding: const EdgeInsets.all(15),
+                    isDense: true,
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            width: 1,
+                            color: CommonColor.REGISTRARTION_COLOR),
+                        borderRadius: BorderRadius.circular(10.0)),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            width: 1,
+                            color: CommonColor.REGISTRARTION_COLOR),
+                        borderRadius: BorderRadius.circular(10.0)),
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            width: 1,
+                            color: CommonColor.REGISTRARTION_COLOR),
+                        borderRadius: BorderRadius.circular(10.0)),
+                    hintStyle: TextStyle(
+                        fontFamily: "Roboto_Regular",
+                        fontSize: SizeConfig.blockSizeHorizontal * 4.0,
+                        color: CommonColor.REGISTRARTION_TRUSTEE
+                    ))
+            ),
+          ),
+        ],
       ),
+
     );
   }
 
   Widget getPasswordField() {
-    return Padding(
-      padding: const EdgeInsets.only(top: 30, left: 15, right: 15),
-      child: TextFormField(
-        controller: passwordController,
-        obscureText: true,
-        decoration: const InputDecoration(
-          label: Text('Password'),
-          border: OutlineInputBorder(),
+    return Form(
+      key: _formKeyPassword,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 30, left: 15, right: 15),
+        child: TextFormField(
+          controller: passwordController,
+          obscureText: true,
+          validator: (String? value) {
+            if (value!.isEmpty) {
+              return 'Password Field Is Required';
+
+            }
+            return null;
+          },
+
+            decoration: InputDecoration(
+                labelText: 'Password',
+
+                contentPadding: const EdgeInsets.all(15),
+                isDense: true,
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        width: 1,
+                        color: CommonColor.REGISTRARTION_COLOR),
+                    borderRadius: BorderRadius.circular(10.0)),
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        width: 1,
+                        color: CommonColor.REGISTRARTION_COLOR),
+                    borderRadius: BorderRadius.circular(10.0)),
+                hintStyle: TextStyle(
+                  fontFamily: "Roboto_Regular",
+                  fontSize: SizeConfig.blockSizeHorizontal * 4.0,
+                  color: CommonColor.REGISTRARTION_TRUSTEE
+                ))
         ),
       ),
     );
@@ -102,18 +239,10 @@ class _LoginScreenState extends State<LoginScreen> {
       padding: EdgeInsets.only(left: 30, right: 30, top: 35),
       child: GestureDetector(
         onTap: () {
-          result = getLoginUsers();
-
-          result?.then((value) {
-              var box = Hive.box(kBoxName);
-          box.put(kToken, value.data?.token);
-          print("token ${box.get("token")}");
-        });
-
-
-        },
+          validate();
+          },
         onDoubleTap: () {},
-        child: Container(
+        child: /*Container(
           height: 50,
           decoration: BoxDecoration(
               gradient: LinearGradient(
@@ -136,9 +265,30 @@ class _LoginScreenState extends State<LoginScreen> {
                 ),),
             ],
           ),
-        ),
+        ),*/
+        Container(
+            height: 43,
+
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                  colors: [CommonColor.LEFT_COLOR, CommonColor.RIGHT_COLOR]),
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: Center(
+              child: Text(
+                "Login",
+                style: TextStyle(
+                    fontFamily: "Roboto_Regular",
+                    fontWeight: FontWeight.w700,
+                    fontSize: SizeConfig.blockSizeHorizontal * 4.5,
+                    color: CommonColor.WHITE_COLOR),
+              ),
+            )),
       ),
-    ) /*Padding(
+    )
+    /*Padding(
       padding: const EdgeInsets.only(top: 50, left: 25, right: 25),
       child: ElevatedButton(
         style: ElevatedButton.styleFrom({
@@ -163,6 +313,10 @@ class _LoginScreenState extends State<LoginScreen> {
         children: [
           const Text("New User Register?", style: TextStyle(
             fontSize: 15,
+              fontFamily: "Roboto_Regular",
+              fontWeight: FontWeight.w400,
+             // fontSize: SizeConfig.blockSizeHorizontal * 4.5,
+              color: CommonColor.BLACK_COLOR
           ),),
 
           GestureDetector(
@@ -176,7 +330,7 @@ class _LoginScreenState extends State<LoginScreen> {
               child: const Text(" Register", style: TextStyle(
                   fontSize: 15,
                   fontWeight: FontWeight.bold,
-                  color: Colors.green
+                  color: CommonColor.RIGHT_COLOR
               ),),
             ),
           ),
@@ -195,8 +349,8 @@ class _LoginScreenState extends State<LoginScreen> {
           });
       print("new user:" + result.body);
       if (result.statusCode == 200) {
-        Navigator.push(context,
-            MaterialPageRoute(builder: (context) => ParentTabBarScreen()));
+       /* Navigator.push(context,
+            MaterialPageRoute(builder: (context) => ParentTabBarScreen()));*/
       }
 
       return userLoginResponseModelFromJson(result.body);
